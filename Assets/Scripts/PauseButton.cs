f528b0c7789eefd13888b05fab0d79bf5651d866
Collin Newman <collinnewman12@gmail.com>
2024-03-01 22:03:59 -0600

    add icons to pause/play button

--- Assets/Scripts/PauseButton.cs
+++ Assets/Scripts/PauseButton.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class PauseButton : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private GameObject playIcon;
+    [SerializeField] private GameObject stopIcon;
+
     private bool isPaused = false;
+
+    public void Start()
+    {
+        playIcon.SetActive(false);
+        stopIcon.SetActive(true);
+    }
 
     public void Pause()
     {
@@ -13,11 +23,19 @@ public class PauseButton : MonoBehaviour
         {
             audioSource.Play();
             isPaused = false;
+
+            // Update Icon
+            playIcon.SetActive(false);
+            stopIcon.SetActive(true);
         }
         else
         {
             audioSource.Pause();
             isPaused = true;
+
+            // Update Icon
+            playIcon.SetActive(true);
+            stopIcon.SetActive(false);
         }
     }
 }
